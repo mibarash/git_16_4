acf9668bffa648fd46d98d5dec11256839fb47aa
mi <mibarash@gmail.com>
2018-05-29 22:03:36 +0300

    dinamic utm glc

--- main.js
+++ main.js
@@ -20,35 +20,35 @@ console.log("hello");
 // ******
 // for dinamic links
 
-// function getUrlParameter(referrers) {
-//     var refferer = false;
-//     for (var i=0;i<referrers.length;i++) {
-//       name = referrers[i];
-//       name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-//       var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-//       var results = regex.exec(location.search);
-//       if ( results != null ) {
-//         refferer = decodeURIComponent(results[1].replace(/\+/g, ' '));
-//       }
-//     }
-//     return refferer;
-//   };
+function getUrlParameter(referrers) {
+    var refferer = false;
+    for (var i=0;i<referrers.length;i++) {
+      name = referrers[i];
+      name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+      var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+      var results = regex.exec(location.search);
+      if ( results != null ) {
+        refferer = decodeURIComponent(results[1].replace(/\+/g, ' '));
+      }
+    }
+    return refferer;
+  };
   
-//   // List all possible refferers that can be in URL:
-//   referrers = ['gclid','utm','MSCLKID','utm_source','msclkid'];
-//   referrer = getUrlParameter(referrers);
-//   // This only works for 1 refferer, there shouldn't be more than 1 type of referrer
-//   if (referrer != false) {
-//     startNowBtn = document.querySelector('.start_now');
-//     siteBtn = document.querySelector('.site_button');
-//     bannerBtn = document.querySelector('.banner_link');
+  // List all possible refferers that can be in URL:
+  referrers = ['gclid','utm','MSCLKID','utm_source','msclkid'];
+  referrer = getUrlParameter(referrers);
+  // This only works for 1 refferer, there shouldn't be more than 1 type of referrer
+  if (referrer != false) {
+    startNowBtn = document.querySelector('.start_now');
+    siteBtn = document.querySelector('.site_button');
+    bannerBtn = document.querySelector('.banner_link');
 
-//     // startNowBtn.href = 'http://wix.com?a=1&b=1&s=' + referrer;
-//     startNowBtn.href = 'http://wixstats.com/?a=16754&c=124&s1=' + referrer;
-//     siteBtn.href = 'http://wixstats.com/?a=16754&c=124&s1=' + referrer;
-//     bannerBtn.href = 'http://wixstats.com/?a=16754&c=124&s1=' + referrer;
+    // startNowBtn.href = 'http://wix.com?a=1&b=1&s=' + referrer;
+    startNowBtn.href = 'http://wixstats.com/?a=16754&c=124&s1=' + referrer;
+    siteBtn.href = 'http://wixstats.com/?a=16754&c=124&s1=' + referrer;
+    bannerBtn.href = 'http://wixstats.com/?a=16754&c=124&s1=' + referrer;
 
-//   }
+  }
 
 
 
